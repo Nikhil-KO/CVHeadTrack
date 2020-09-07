cea04078e4039386055874a6dda30bbabf91b82f
Nick <33524818+Nikhil-KO@users.noreply.github.com>
2020-09-07 20:06:17 +0100

    Add the SimConnect dll file and append it to post build script. Button to link application to game successfull.

--- CVHeadTrack/MainWindow.xaml.cs
+++ CVHeadTrack/MainWindow.xaml.cs
@@ -12,14 +12,49 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Microsoft.FlightSimulator.SimConnect;
+using System.Runtime.InteropServices;
+
 
 namespace CVHeadTrack {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window {
+
+        private IntPtr simConnectHandle;
+
         public MainWindow() {
             InitializeComponent();
+        }
+
+        private void DebugDialog(String msg) {
+            TextBlock myTextBlock = (TextBlock)this.FindName("debugPanel");
+            myTextBlock.Text = msg;
+
+        }
+
+        private void LinkFlightSim(object sender, RoutedEventArgs e) {
+            DebugDialog("Starting to link");
+            // Open
+            // Declare a SimConnect object
+            SimConnect simconnect = null;
+            // User-defined win32 event
+            const int WM_USER_SIMCONNECT = 0x0402;
+            bool success = true;
+            try {
+                simconnect = new SimConnect("Managed Data Request", this.simConnectHandle, WM_USER_SIMCONNECT, null, 0);
+            } catch (COMException ex) {
+                success = false;
+                DebugDialog("Failed to open connection " + ex.Message);
+                if (simconnect != null) {
+                    simconnect.Dispose();
+                    simconnect = null;
+                }
+            }
+            if (success) {
+                DebugDialog("Attached to game!");
+            }
         }
     }
 }
--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # CVHeadTrack
 Custom made, Computer Vision based head tracking software made using OpenCV and SimConnect API for Flight Simulator 2020.
+
+C# Facial landmarks:
+https://medium.com/machinelearningadvantage/detect-facial-landmark-points-with-c-and-dlib-in-only-50-lines-of-code-71ab59f8873f
