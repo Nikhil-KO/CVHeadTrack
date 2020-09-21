1053fb97b5eb3ffe22ea43aa317b3ec4277276f8
Nick <33524818+Nikhil-KO@users.noreply.github.com>
2020-09-21 23:22:09 +0100

    add moving average class

---
+++ CVHeadTrack/MovingAverage.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CVHeadTrack {
+
+    class MovingAverage {
+
+        private double[] buffer;
+        private int index;
+
+        public MovingAverage(int size) {
+            this.buffer = new double[size];
+            this.index = 0;
+        }
+
+        public void Add(double item) {
+            this.buffer[this.index] = item;
+            this.index = (this.index + 1) % this.buffer.Length;
+        }
+
+        public double Average() {
+            double sum = 0;
+            foreach (double item in buffer) {
+                sum += item;
+            }
+            return (sum/buffer.Length);
+        }
+
+    }
+}
